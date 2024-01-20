41f86c05b66907dadaf9064ac68b872c0aeefe2b
Kabir1430 <104447995+Kabir1430@users.noreply.github.com>
2024-01-20 21:29:56 +0530

    Update Gun.cs

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -5,14 +5,5 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
