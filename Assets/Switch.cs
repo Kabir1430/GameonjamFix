84b6aceddd7bf863d3f5d959207d901de44be304
Kabir1430 <104447995+Kabir1430@users.noreply.github.com>
2024-01-21 23:44:16 +0530

    j

--- Assets/Switch.cs
+++ Assets/Switch.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
-    \
+
+
+
+
+    private void Start()
+    {
+        
+    StartCoroutine(Check());
+
+    }
+
+
+
+      IEnumerator Check()
+
+    {
+
+        yield return new WaitForSeconds(24);
+
+
+        SceneManager.LoadScene(3);
+    }
 
 
     // Start is called before the first frame update
